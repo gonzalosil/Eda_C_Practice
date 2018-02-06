2a1671e7e9d19e982d6914d24ca5dafc86c42abc
Gonzalo Silva <gonzalosilva@gonzalos-macbook-pro.local>
2018-02-05 21:26:11 -0300

    Initial Commit

---
+++ Eda_C_Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Eda_C_Practice
+//
+//  Created by Gonzalo Silva on 05/02/2018.
+//  Copyright © 2018 Gonzalo Silva. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
